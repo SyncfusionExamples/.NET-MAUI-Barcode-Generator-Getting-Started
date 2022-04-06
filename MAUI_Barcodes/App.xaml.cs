ed1b5ab738ed836d9a0560c7553655c6d4ac95fc
sheik syed abthaheer <sheiksyedm@syncfusion.com>
2022-04-06 15:57:37 +0530

    Barcode sample added.

---
+++ MAUI_Barcodes/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_Barcodes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MAUI_Barcodes/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Barcodes;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
